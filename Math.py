3abfffb8e41c8aff12f3fc4a87dd069e6f844316
DICE <Umair.khalid1234567@gmail.com>
2022-10-18 14:50:13 +0100

    Full working code

---
+++ Math.py
@@ -0,0 +1,44 @@
+#Developer : Umair Khalid
+#University Id: 210051122
+# This function test the sum function implementation.
+
+def maxValue(value1, value2):
+    return max(value1,value2)
+
+def minValue(value1,value2):
+    return min(value1,value2)
+
+def addValue(value1,value2):
+    return value1 + value2
+
+def subValue(value1,value2):
+    return value1 - value2
+
+def multiValue(value1,value2):
+    return value1 * value2
+
+def divideValue(value1,value2):
+    return value1 / value2
+
+def power(value1,value2):
+    return pow(value1,value2)
+
+def mod(value1,value2):
+    return value1 % value2
+
+print(maxValue(2,3))
+print('----------------------------------------------------------------')
+print(minValue(2,3))
+print('----------------------------------------------------------------')
+print(addValue(2,3))
+print('----------------------------------------------------------------')
+print(subValue(2,3))    
+print('----------------------------------------------------------------')
+print(multiValue(2,3))
+print('----------------------------------------------------------------')
+print(divideValue(2,3))
+print('----------------------------------------------------------------')
+print(power(2,3))
+print('----------------------------------------------------------------')
+print(mod(2,3))
+      
