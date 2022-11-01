0090f69577231490d1dba78a133f9d757990c6b1
WanisMousa <210156346@aston.ac.uk>
2022-11-01 15:48:04 +0000

    Login page for MW4U

---
+++ MW4U/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class= "title">
+        <h1>Login</h1>
+        </div>
+        <div class ="login">
+            <form action ="" method ="POST">
+                <label>Email Address:</label><br><input type="email" name="email" placeholder="Email Address" required><br>
+                <label>Password:</label><br><input type="password" name="password" placeholder="Password" required><br><br>
+                <div class = "sub">
+                <button type = "submit" name = "login">Login</button>
+            </div>
+            </form>
+        </div>
+        <div class ="info">
+            <p>Not a User?</p>
+            <a href="" >Register now!</a>
+        </div>
+</body>
+</html>
